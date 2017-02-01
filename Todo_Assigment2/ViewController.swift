18774c89c99ea3706780df5cf0b75cb5276906b8
prashant pol <prashantn.pol@gmail.com>
2017-01-31 20:05:03 -0500

    Added custom cell

--- Todo_Assigment2/ViewController.swift
+++ Todo_Assigment2/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource {
 
+    let data: [String]=["Task1","Task2","Task3"]
+    
+    @IBOutlet weak var lbl_todo: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    @IBAction func btnSwtich(_ sender: UISwitch) {
+        
+        print("test")
+    }
+    
 
+    //edit button touched event
+    @IBAction func btnEditTouched(_ sender: UIButton) {
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    //loading data
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")! as UITableViewCell
+        
+        //cell.textLabel?.text = self.items[indexPath.row]
+      //  int row = indexPath.row
+        cell.textLabel?.text=data[indexPath.row]
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
 }
 
